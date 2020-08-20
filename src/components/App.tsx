04a2b03a6c00e465f56ab67d4bb5e2c5f29e3cb0
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-08-20 15:40:40 -0700

    Sorted communities alphabetacally

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,9 @@ import { CommunityList } from "./CommunityList";
 const communityUrl =
   "https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/communities";
 
+const homeUrl =
+  "https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io/homes";
+
 interface Community {
   id: string;
   name: string;
@@ -16,8 +19,20 @@ interface Community {
 
 interface CommunitiesList extends Array<Community> {}
 
+// Todo: change type of type
+interface Home {
+  id: string;
+  communityId: string;
+  price: number;
+  area: number;
+  type: string;
+}
+
+interface HomesList extends Array<Home> {}
+
 export default function App() {
   const [communities, setCommunities] = useState<CommunitiesList | null>(null);
+  const [homes, setHomes] = useState<HomesList | null>(null);
 
   useEffect(() => {
     //Fetch communities data
@@ -25,13 +40,18 @@ export default function App() {
       const communitiesList = res.data as CommunitiesList;
       setCommunities(communitiesList);
     });
+    //Fetch homes data
+    axios.get(homeUrl).then((res: AxiosResponse) => {
+      const homesList = res.data as HomesList;
+      setHomes(homesList);
+    });
   }, []);
 
   return (
     <div>
       <Container>
-        {communities ? (
-          <CommunityList communities={communities} />
+        {communities && homes ? (
+          <CommunityList communities={communities} homes={homes} />
         ) : (
           <div>Loading...</div>
         )}
--- src/components/CommunityList.tsx
+++ src/components/CommunityList.tsx
@@ -11,17 +11,40 @@ interface Community {
 
 interface CommunitiesList extends Array<Community> {}
 
+interface Home {
+  id: string;
+  communityId: string;
+  price: number;
+  area: number;
+  type: string;
+}
+
+interface HomesList extends Array<Home> {}
+
 interface CommunityListProps {
   communities: CommunitiesList;
+  homes: HomesList;
 }
 
 export const CommunityList = (props: CommunityListProps): JSX.Element => {
   return (
     <div>
       {props.communities ? (
-        props.communities.map((community) => {
-          return <Community community={community} key={community.id} />;
-        })
+        props.communities
+          .sort((a: Community, b: Community) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            let result = 0;
+            if (nameA > nameB) {
+              result = 1;
+            } else if (nameA < nameB) {
+              result = -1;
+            }
+            return result;
+          })
+          .map((community) => {
+            return <Community community={community} key={community.id} />;
+          })
       ) : (
         <div>Loading...</div>
       )}
